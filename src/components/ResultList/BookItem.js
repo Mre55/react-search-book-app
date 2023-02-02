9317459226e7469ea80a1c96567f74330a8765c4
mre55 <misganawmihreteab>
2023-02-02 12:23:32 +0300

    Render card for one book item and add style to the card

--- src/components/ResultList/BookItem.js
+++ src/components/ResultList/BookItem.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const BookItem = () => {
+import classes from "./ResultList.module.css";
+
+const BookItem = (props) => {
   return (
-    <div>BookItem</div>
-  )
-}
+    <li key={props.id} className={classes.book}>
+      <div className={classes["book-img-title"]}>
+        <div>
+          <img
+            className={classes["book-img"]}
+            src={props?.image["image/jpeg"]}
+            alt={`${props.title}`}
+          />
+        </div>
+        <div>
+          <h3 className={classes.title}>{props.title}</h3>
+          <p>
+            By:{" "}
+            {props.authors
+              .map((author) => author["name"].replace(",", " "))
+              .join(", ")}
+          </p>
+        </div>
+      </div>
+      {props.isPicked ? (
+        <button
+          className={classes["picked-btn"]}
+          onClick={props.onAdd.bind(null, props.id)}
+          disabled
+        >
+          Pick
+        </button>
+      ) : (
+        <button
+          className={classes["not-picked-btn"]}
+          onClick={props.onAdd.bind(null, props.id)}
+        >
+          Pick
+        </button>
+      )}
+    </li>
+  );
+};
 
-export default BookItem
+export default BookItem;
