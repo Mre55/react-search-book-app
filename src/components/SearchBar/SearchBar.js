33d135f14645d1e5f3074a12ec3f0d87041aabbb
mre55 <misganawmihreteab>
2023-02-02 12:18:56 +0300

    Add Input filed and button with their attribute and add style for them

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SearchBar = () => {
+import classes from "./SearchBar.module.css";
+
+const SearchBar = (props) => {
+  const [book, setBook] = useState("");
+
+  const handleChange = (event) => {
+    setBook(event.target.value);
+  };
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className={classes["search-bar-container"]}>
+      <input
+        className={classes.input}
+        type="text"
+        placeholder="Search for..."
+        onChange={handleChange}
+      />
+      <button
+        className={classes.searchBtn}
+        onClick={props.onFetch.bind(null, book)}
+      >
+        Search
+      </button>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
