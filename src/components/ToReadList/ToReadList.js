3b1cdbac723724756e8ee0ff539f3fd416c0b20f
mre55 <misganawmihreteab>
2023-02-02 12:24:51 +0300

    Render To be read books as card and add style for the card

--- src/components/ToReadList/ToReadList.js
+++ src/components/ToReadList/ToReadList.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const ToReadList = () => {
+import classes from "./ToReadList.module.css";
+
+const ToReadList = (props) => {
+  const readList = props.toReadList.filter((book) => book.picked === true);
+
   return (
-    <div>ToReadList</div>
-  )
-}
+    <div className={classes["book-to-read-container"]}>
+      <p className={classes["book-to-read-title"]}>To read list</p>
+      <ul className={classes["book-to-read-list"]}>
+        {readList.map((book) => (
+          <li key={book.id} className={classes["book-to-read"]}>
+            <div className={classes["book-img-title"]}>
+              <div>
+                <img
+                  className={classes["book-img"]}
+                  src={book?.image["image/jpeg"]}
+                  alt={`${book.title}`}
+                />
+              </div>
+              <div>
+                <h5 className={classes.title}>{book.title}</h5>
+              </div>
+            </div>
+            <button
+              className={classes["pick-btn"]}
+              onClick={props.onRemove.bind(null, book.id)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default ToReadList
+export default ToReadList;
