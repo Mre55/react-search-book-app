f0db48917610f31896104fae2e7a5d0445363824
mre55 <misganawmihreteab>
2023-02-02 12:22:23 +0300

    Pass props for BooItem component and add style form them

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const ResultList = () => {
+import BookItem from "./BookItem";
+import classes from "./ResultList.module.css";
+
+const ResultList = (props) => {
+  const listOfBooks = props.resultList.map((book) => (
+    <BookItem
+      key={book.id}
+      id={book.id}
+      title={book?.title}
+      authors={book?.authors}
+      image={book.image}
+      isPicked={book.picked}
+      onAdd={props.onAdd}
+    />
+  ));
+
   return (
-    <div>ResultList</div>
-  )
-}
+    <div className={classes["all-books"]}>
+      <ul className={classes["list-of-books"]}>{listOfBooks}</ul>
+    </div>
+  );
+};
 
-export default ResultList
+export default ResultList;
