96ca154627cd0dca0b243ae2dab0abdfbc8f705d
mre55 <misganawmihreteab>
2023-02-02 12:27:16 +0300

    Add fetchBook function to fetch book using the key word

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import ResultList from "./components/ResultList/ResultList";
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -6,6 +7,38 @@ import ToReadList from "./components/ToReadList/ToReadList";
 import classes from "./App.module.css";
 
 function App() {
+  const [bookList, setBookList] = useState([]);
+
+  const fetchBooksHandler = (book) => {
+    if (book === null) {
+      return;
+    }
+
+    if (book.trim() === "") {
+      return;
+    }
+
+    const trimedInputValue = book.trim().replace(/\s/g, "%20");
+
+    axios
+      .get(`https://gutendex.com/books/?search=${trimedInputValue}`)
+      .then((res) => {
+        const response = res.data;
+
+        const loadedBooks = [];
+
+        for (const key in response.results) {
+          loadedBooks.push({
+            id: key,
+            title: response.results[key].title,
+            authors: response.results[key].authors,
+            image: response.results[key].formats,
+          });
+        }
+      })
+      .catch((error) => {});
+  };
+
   return (
     <div className={classes.container}>
       <SearchBar />
